38c948272c3b35dd08aec3ffb2722dd6082fc324
Renaud Gaspard <gaspardrenaud@hotmail.com>
2019-03-11 09:26:37 +0100

    removed less and updated plugin mixins

--- octoprint_marlin_flasher/__init__.py
+++ octoprint_marlin_flasher/__init__.py
@@ -2,47 +2,34 @@
 from __future__ import absolute_import
 import octoprint.plugin
 
-class MarlinFlasherPlugin(octoprint.plugin.SettingsPlugin,
-                           octoprint.plugin.AssetPlugin,
-                           octoprint.plugin.TemplatePlugin):
 
-	##~~ SettingsPlugin mixin
+class MarlinFlasherPlugin(octoprint.plugin.SettingsPlugin,
+							octoprint.plugin.AssetPlugin,
+							octoprint.plugin.TemplatePlugin):
 
 	def get_settings_defaults(self):
 		return dict(
-			# put your plugin's default settings here
+			arduino_path=None
 		)
-
-	##~~ AssetPlugin mixin
 
 	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
 		return dict(
 			js=["js/marlin_flasher.js"],
-			css=["css/marlin_flasher.css"],
-			less=["less/marlin_flasher.less"]
+			css=["css/marlin_flasher.css"]
 		)
 
-	##~~ Softwareupdate hook
-
 	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-		# for details.
 		return dict(
 			marlin_flasher=dict(
-				displayName="Marlin_flasher Plugin",
+				displayName="Marlin Flasher",
 				displayVersion=self._plugin_version,
 
-				# version check: github repository
 				type="github_release",
-				user="you",
+				user="Renaud11232",
 				repo="OctoPrint-Marlin-Flasher",
 				current=self._plugin_version,
 
-				# update method: pip
-				pip="https://github.com/you/OctoPrint-Marlin-Flasher/archive/{target_version}.zip"
+				pip="https://github.com/Renaud11232/OctoPrint-Marlin-Flasher/archive/{target_version}.zip"
 			)
 		)
 
