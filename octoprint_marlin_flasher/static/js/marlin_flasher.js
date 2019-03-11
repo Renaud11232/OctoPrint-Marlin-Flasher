eda43c999a77c2812c80030a94a4e142dc5a8d09
Renaud Gaspard <gaspardrenaud@hotmail.com>
2019-03-11 11:52:43 +0100

    wizard and config page done

--- octoprint_marlin_flasher/static/js/marlin_flasher.js
+++ octoprint_marlin_flasher/static/js/marlin_flasher.js
@@ -2,17 +2,8 @@ $(function() {
     function MarlinFlasherViewModel(parameters) {
         var self = this;
 
-        // assign the injected parameters, e.g.:
-        // self.loginStateViewModel = parameters[0];
-        // self.settingsViewModel = parameters[1];
-
-        // TODO: Implement your plugin's view model here.
     }
 
-    /* view model class, parameters for constructor, container to bind to
-     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
-     * and a full list of the available options.
-     */
     OCTOPRINT_VIEWMODELS.push({
         construct: MarlinFlasherViewModel,
         // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
