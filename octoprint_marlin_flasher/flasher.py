b67999f42daedd7d2b6a24cd104b15fdf7c18748
Renaud Gaspard <gaspardrenaud@hotmail.com>
2019-10-29 16:14:07 +0100

    changed implementation detection logic

--- octoprint_marlin_flasher/flasher.py
+++ octoprint_marlin_flasher/flasher.py
@@ -9,9 +9,10 @@ class BaseFlasher:
 	def __init__(self, settings, printer):
 		self._settings = settings
 		self._printer = printer
+		self._firmware = None
 
 	def handle_upload(self):
-		pass
+		raise FlasherError("Unsupported function call.")
 
 	def flash(self):
 		raise FlasherError("Unsupported function call.")
@@ -20,23 +21,25 @@ class BaseFlasher:
 		raise FlasherError("Unsupported function call.")
 
 
-class MarlinFlasher(BaseFlasher):
+class MarlinFlasher(ArduinoFlasher, PlatformIOFlasher):
 
 	def __init__(self, settings, printer):
-		BaseFlasher.__init__(self, settings, printer)
-		self.__arduino = ArduinoFlasher(settings, printer)
-		self.__platformio = PlatformIOFlasher(settings, printer)
+		ArduinoFlasher.__init__(self, settings, printer)
+		PlatformIOFlasher.__init__(self, settings, printer)
 
 	def __get_implementation(self):
 		platform = self._settings.get(["platform_type"])
 		if platform == PlatformType.ARDUINO:
-			return self.__arduino
+			return ArduinoFlasher
 		elif platform == PlatformType.PLATFORM_IO:
-			return self.__platformio
+			return PlatformIOFlasher
 		raise FlasherError("Unknown firmware platform")
 
+	def handle_upload(self):
+		return self.__get_implementation().handle_upload(self)
+
 	def flash(self):
-		return self.__get_implementation().flash()
+		return self.__get_implementation().flash(self)
 
 	def compile(self):
-		return self.__get_implementation().compile()
+		return self.__get_implementation().compile(self)
