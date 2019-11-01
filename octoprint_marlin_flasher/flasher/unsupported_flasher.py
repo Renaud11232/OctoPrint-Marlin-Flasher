da94f88ef75ee936a8f61628648c7a23908bc31e
Renaud Gaspard <gaspardrenaud@hotmail.com>
2019-11-01 11:41:35 +0100

    avoid crash with unknown platform

--- octoprint_marlin_flasher/flasher/unsupported_flasher.py
+++ octoprint_marlin_flasher/flasher/unsupported_flasher.py
@@ -1,5 +1,10 @@
 from .base_flasher import BaseFlasher
+from flask_babel import gettext
 
 
 class UnsupportedFlasher(BaseFlasher):
-	pass
+
+	def check_setup_errors(self):
+		return dict(
+			error=gettext("Unkown platform, please check your settings.")
+		)
