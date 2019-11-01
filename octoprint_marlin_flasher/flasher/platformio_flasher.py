6d18f8c20bc9e7e8efbce9a371923b65ce444f64
Renaud Gaspard <gaspardrenaud@hotmail.com>
2019-11-01 11:43:13 +0100

    avoid crash with plaftorm io platform

--- octoprint_marlin_flasher/flasher/platformio_flasher.py
+++ octoprint_marlin_flasher/flasher/platformio_flasher.py
@@ -1,5 +1,11 @@
 from .base_flasher import BaseFlasher
+from flask_babel import gettext
 
 
 class PlatformIOFlasher(BaseFlasher):
-	pass
+
+	def check_setup_errors(self):
+		return dict(
+			# TODO not done yet
+			error=gettext("Not implemented yet")
+		)
