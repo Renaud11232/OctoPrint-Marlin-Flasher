0dd1ed360c669ae49312eb93735e6a3cc44dd57d
Renaud Gaspard <gaspardrenaud@hotmail.com>
2020-04-18 12:35:38 +0200

    version bump + little css improvement

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 plugin_identifier = "marlin_flasher"
 plugin_package = "octoprint_marlin_flasher"
 plugin_name = "OctoPrint-Marlin-Flasher"
-plugin_version = "0.3.5"
+plugin_version = "0.4.0rc1"
 plugin_description = """Plugin that allows you to flash your printer with the latest Marlin version, or Arduino code"""
 plugin_author = "Renaud Gaspard"
 plugin_author_email = "gaspardrenaud@hotmail.com"
